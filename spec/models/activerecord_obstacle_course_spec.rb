a3c406c37298064b0c4092972f940c5ae9253d16
jtrtj <jt.r@live.com>
2018-07-25 10:22:12 -0600

    take out nonworking solution on twenty-sixth

--- spec/models/activerecord_obstacle_course_spec.rb
+++ spec/models/activerecord_obstacle_course_spec.rb
@@ -480,18 +480,17 @@ describe 'ActiveRecord Obstacle Course' do
     expected_result = ['Thing 1', 'Thing 2', 'Thing 3', 'Thing 4', 'Thing 5', 'Thing 7', 'Thing 8', 'Thing 9', 'Thing 10']
 
     # ----------------------- Using Ruby -------------------------
-    items = Item.all
+    # items = Item.all
 
-    ordered_items = items.map do |item|
-      item if item.orders.present?
-    end.compact
+    # ordered_items = items.map do |item|
+    #   item if item.orders.present?
+    # end.compact
 
-    ordered_items_names = ordered_items.map(&:name)
+    # ordered_items_names = ordered_items.map(&:name)
     # ------------------------------------------------------------
 
     # ------------------ ActiveRecord Solution ---------------------- 26
-    ordered_items = Item.distinct.joins(:order_items).pluck(:name)
-    ordered_items = Item.distinct.joins(:orders).name
+    ordered_items_names = Item.distinct.joins(:order_items).pluck(:name) 
     # When you find a solution, experiment with adjusting your method chaining
     # Which ones are you able to switch around without relying on Ruby's Enumerable methods?
     # --------------------------------------------------------------- 26
